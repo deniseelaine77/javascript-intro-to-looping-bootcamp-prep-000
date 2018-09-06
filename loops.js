6e5e29280f1cae36995d193a0a9c7c7623d3af9d
dehast59@gmail.com <dehast59@gmail.com>
2018-09-06 23:07:54 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array){
+  for(let i = 0; i <25; i++){
+    if(i === 1){
+      array.push("I am 1 strange loop.") 
+      } else {
+        array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+      }
+    }
+    return array
+  }
+
+function whileLoop(n){
+  let countdown = n;
+  while (countdown > 0) {
+    console.log(-- countdown);
+  }
+  return "done"
+}
+
+// function doWhileLoop(array){
+//   var i = array.length; 
+//   function incrementVariable() {
+//     i - 1;
+//   }
+// do {
+//   array.pop;
+// } while (array.length > 0);
+// }
